6992b5c4f85b3adcd2acddb5a779bfd0d63d771d
Zairo Bastos <zairo_vianahd@hotmail.com>
2022-12-30 18:56:26 -0300

    feat: :memo: Ajuste no componente do Texto

--- apitofinal3-front/components/Text/index.tsx
+++ apitofinal3-front/components/Text/index.tsx
@@ -4,8 +4,25 @@ import clsx from "clsx";
 export interface dataText {
 	children: ReactNode;
 	asChild: boolean;
-	fontColor?: string;
-	fontSize: string;
+	fontColor?:
+		| "text-black-900"
+		| "text-black-100"
+		| "text-gray-900"
+		| "text-gray-500"
+		| "text-green-100"
+		| "text-green-800"
+		| "text-white";
+	fontSize:
+		| "text-sm"
+		| "text-base"
+		| "text-xl"
+		| "text-1xl"
+		| "text-2xl"
+		| "text-3xl"
+		| "text-4xl"
+		| "text-42xl"
+		| "text-42px"
+		| "text-5xl";
 	lineHeight?: string;
 	fontWeight?: string;
 	textAlign?: "center" | "right" | "left" | "justify";
