f4c05f11aa3736e7162d91909e2ca9e81001d0f8
Zairo Bastos <zairo_vianahd@hotmail.com>
2023-01-01 12:16:38 -0300

    feat: :zap: Tela de login falta apenas algumas lincagens

--- apitofinal3-front/pages/login/index.tsx
+++ apitofinal3-front/pages/login/index.tsx
@@ -5,6 +5,7 @@ import { Input2 } from "../../components/Input";
 import Label from "../../components/Input/label";
 import { Eye, EyeSlash, Envelope } from "phosphor-react";
 import { useState } from "react";
+import Link from "next/link";
 const Login = () => {
 	const [click, setClick] = useState(true);
 	return (
@@ -12,17 +13,19 @@ const Login = () => {
 			<div className="w-full flex flex-row flex-wrap">
 				<div className="w-1/2 bg-white h-screen px-8 flex flex-col flex-wrap gap-8">
 					<header className="mt-14 flex flex-col flex-wrap gap-5">
-						<figure className="flex flex-row flex-wrap items-center gap-6">
-							<Image src={Logo} alt="logo do apito final" />
-							<Text
-								fontSize="text-6xl"
-								fontColor="text-green-100"
-								fontWeight="font-bold"
-								lineHeight="leading-12"
-							>
-								<h1>Apito Final</h1>
-							</Text>
-						</figure>
+						<Link href="/">
+							<figure className="flex flex-row flex-wrap items-center gap-6">
+								<Image src={Logo} alt="logo do apito final" />
+								<Text
+									fontSize="text-6xl"
+									fontColor="text-green-100"
+									fontWeight="font-bold"
+									lineHeight="leading-12"
+								>
+									<h1>Apito Final</h1>
+								</Text>
+							</figure>
+						</Link>
 						<div>
 							<Text fontSize="text-42xl" lineHeight="leading-10">
 								<h3>Bem-vindo ao Apito Final</h3>
@@ -69,6 +72,38 @@ const Login = () => {
 								</div>
 							</Input2.Root>
 						</div>
+						<Text
+							fontSize="text-base"
+							textAlign="right"
+							fontColor="text-green-800"
+							lineHeight="leading-6"
+						>
+							<p>Esqueceu a senha?</p>
+						</Text>
+						<button className="bg-green-800 rounded py-3">
+							<Text
+								fontSize="text-xl"
+								fontWeight="font-semibold"
+								fontColor="text-white"
+								lineHeight="leading-6"
+								textAlign="center"
+							>
+								<p>Entrar</p>
+							</Text>
+						</button>
+						<Text
+							fontSize="text-base"
+							textAlign="left"
+							fontColor="text-gray-700"
+							lineHeight="leading-6"
+						>
+							<p>
+								Ainda não tem conta?{" "}
+								<span className="text-green-800">
+									Criar Conta
+								</span>
+							</p>
+						</Text>
 					</form>
 				</div>
 				<figure className="w-1/2 fixed right-0 h-screen fundo"></figure>
